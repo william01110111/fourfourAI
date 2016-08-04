54a46410521120e56474ba6b26cd00cf3d232c39
William W Wold <william01110111@gmail.com>
2016-08-04 14:45:54 -0700

    Initial Commit

---
+++ fourfourAI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fourfourAI
+//
+//  Created by William Wold on 8/4/16.
+//  Copyright © 2016 Widap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
